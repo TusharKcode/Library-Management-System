15b707a44e095c8278b69897697745929518a4d4
Tushar Kumar <tkgoyal104@gmail.com>
2025-08-13 17:58:59 +0530

    Files updated

--- static/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    fetch('/static/includes/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Navbar load error:', error))
-});
